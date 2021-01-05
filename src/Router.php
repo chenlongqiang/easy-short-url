ef9ceaa1b72e3ec45510eedbc0658301b6b63f71
longqiang.chen <longqiang.chen@fanli.com>
2021-01-05 19:24:47 +0800

    desc: optimize exception type: feat

--- src/Router.php
+++ src/Router.php
@@ -8,6 +8,8 @@
 
 namespace EasyShortUrl;
 
+use EasyShortUrl\Exception\EasyShortUrlException;
+
 class Router
 {
     private $uri;
@@ -20,8 +22,6 @@ class Router
     /**
      * 路由分发
      * router dispatch
-     * @throws Exception\EasyShortUrlException
-     * @throws \Psr\Cache\InvalidArgumentException
      */
     public function dispatch()
     {
@@ -34,7 +34,6 @@ class Router
     }
     
     /**
-     * web 管理页
      * web admin
      */
     public function web_admin()
@@ -48,76 +47,61 @@ class Router
      */
     public function api_gen()
     {
-        if ($_POST['type'] == 'to_short') {
-            $longUrl = urldecode($_POST['content']);
-            if (
-                empty($_POST['content'])
-                || (strpos($longUrl, 'http://') !== 0 && strpos($longUrl, 'https://') !== 0)
-            ) {
-                api_error('error scheme, please start with http:// or https://');
-            }
-            
-            try {
+        try {
+            if ($_POST['type'] == 'to_short') {
+                $longUrl = urldecode($_POST['content']);
+                if (
+                    empty($_POST['content'])
+                    || (strpos($longUrl, 'http://') !== 0 && strpos($longUrl, 'https://') !== 0)
+                ) {
+                    throw new EasyShortUrlException('error scheme, please start with http:// or https://');
+                }
                 $shortUrl = (EasyShortUrl::getInstance())->toShort($longUrl, $_POST['access_key']);;
                 api_success($shortUrl);
-            } catch (\Exception $e) {
-                api_error($e->getMessage());
-            }
-        } elseif ($_POST['type'] == 'to_long') {
-            $code = trim(parse_url(urldecode($_POST['content']), PHP_URL_PATH), '/');
-            try {
+            } elseif ($_POST['type'] == 'to_long') {
+                $code = trim(parse_url(urldecode($_POST['content']), PHP_URL_PATH), '/');
                 $longUrl = (EasyShortUrl::getInstance())->toLong($code);;
                 api_success($longUrl);
-            } catch (\Exception $e) {
-                api_error($e->getMessage());
+            } else {
+                throw new EasyShortUrlException('api not found');
             }
-        } else {
-            api_error('api not found');
+        } catch (\Exception $e) {
+            api_error($e->getMessage());
         }
     }
     
     /**
      * 跳转长网址
      * redirect to long url
-     * @throws Exception\EasyShortUrlException
-     * @throws \Psr\Cache\InvalidArgumentException
      */
     public function redirectLongUrl()
     {
-        if (empty($this->uri)) {
-            header("HTTP/1.1 404 Not Found");
-            exit;
-        }
-    
-        if (env('CACHE_OPEN') === '1') {
-            $cacheKey = Cache::key('mapping_code_and_long_url', ['code' => $this->uri]);
-            $cache = Cache::client();
-            $mapping = $cache->getItem($cacheKey);
-            if (!$mapping->isHit()) {
-                // 元素在缓存中不存在，被动缓存
-                try {
+        try {
+            if (env('CACHE_OPEN') === '1') {
+                $cacheKey = Cache::key('mapping_code_and_long_url', ['code' => $this->uri]);
+                $cache = Cache::client();
+                $mapping = $cache->getItem($cacheKey);
+                if (!$mapping->isHit()) {
+                    // 元素在缓存中不存在，被动缓存
                     $longUrl = (EasyShortUrl::getInstance())->toLong($this->uri);;
                     $mapping->set($longUrl);
                     $cache->save($mapping);
-                } catch (\Exception $e) {
-                    header("HTTP/1.1 404 Not Found");
-                    exit;
+                } else {
+                    $longUrl = $mapping->get();
                 }
-            } else {
-                $longUrl = $mapping->get();
             }
-        } 
-        
-        // 未启用缓存或从缓存取出失败
-        if (!isset($longUrl) || empty($longUrl)) {
-            try {
+    
+            // 未启用缓存或从缓存取出失败
+            if (!isset($longUrl) || empty($longUrl)) {
                 $longUrl = (EasyShortUrl::getInstance())->toLong($this->uri);;
-            } catch (\Exception $e) {
-                header("HTTP/1.1 404 Not Found");
-                exit;
             }
+    
+            header('Location:' . $longUrl, true, 302);
+            exit;
+        } catch (\Exception $e) {
+            // todo write log
+            header("HTTP/1.1 404 Not Found");
+            exit;
         }
-
-        header('Location:' . $longUrl, true, 302);
     }
 }
